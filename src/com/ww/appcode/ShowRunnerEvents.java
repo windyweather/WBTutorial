58ab5bc5df9e3c8645f441bd64ea1a1aaefeac61
Windy Player <windyplayer@gmx.com>
2020-04-18 09:08:59 -0700

    Event dispatcher and Window Close handling

--- src/com/ww/appcode/ShowRunnerEvents.java
+++ src/com/ww/appcode/ShowRunnerEvents.java
@@ -4,6 +4,10 @@
 package com.ww.appcode;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 
 import javax.swing.DefaultListModel;
 
@@ -14,7 +18,7 @@ import com.ww.views.*;
  * @author Darrell
  *
  */
-public class ShowRunnerEvents extends FirstWbGui {
+public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 	
 	// get rid of a warning about serialization.
 	private static final long serialVersionUID = 19837503L;
@@ -94,9 +98,34 @@ public class ShowRunnerEvents extends FirstWbGui {
 		showList.addElement("France");
 		showList.addElement("Great Britain");
 		showList.addElement("Japan");
+		showList.addElement("USA");
+		showList.addElement("India");
+		showList.addElement("Vietnam");
+		showList.addElement("Canada");
+		showList.addElement("Denmark");
+		showList.addElement("France");
+		showList.addElement("Great Britain");
+		showList.addElement("Japan");
+		showList.addElement("USA");
+		showList.addElement("India");
+		showList.addElement("Vietnam");
+		showList.addElement("Canada");
+		showList.addElement("Denmark");
+		showList.addElement("France");
+		showList.addElement("Great Britain");
+		showList.addElement("Japan");
+		showList.addElement("USA");
+		showList.addElement("India");
+		showList.addElement("Vietnam");
+		showList.addElement("Canada");
+		showList.addElement("Denmark");
+		showList.addElement("France");
+		showList.addElement("Great Britain");
+		showList.addElement("Japan");
 		showList.addElement("First Show what happens with very very very very long strings in the list what if there were a very long path here in the list");
 
 		System.out.println( "showList stuff added size "+ showList.size() );
+		System.out.println( "showList capacity "+ showList.capacity() );
 		showList.removeAllElements(); // leave the list empty
 		System.out.println( "showList empty list size "+ showList.size() );
 
@@ -104,5 +133,70 @@ public class ShowRunnerEvents extends FirstWbGui {
 		
 	}
 	
+	
+	// catch the window closing event
+	// passed here with a nightmare, but hey, that's Java and AWT/Swing for you.
+	@Override
+	public void windowClosingEvent(WindowEvent e) {
+		
+		System.out.println( "windowClosing - save your stuff here" );
 
+		// not necessary, we are closing, not closed
+		// the framework will exit for us. No need to hammer
+		// things down here.
+	    //System.exit(0);
+	 
+	}
+	
+	/**
+	* Catch all the events here in the child class
+	* use the same actionPerformed listener for every action
+	* Qt has it's problems, but at least you don't have to write
+	* a command dispatcher. Here in Java, it looks like the easiest way
+	* to deal with events from a GUI is to use setActionCommand("string") and
+	* then to dispatch those commands here.
+	* 
+	* In Qt, the dispatcher is built into the framework, so you don't need to build
+	* a dispatcher based on text string. Oh Well. Deployment in Qt is a nightmare,
+	* which is why I'm here, and this is much less of a problem than the Qt Deployment
+	* issue.
+	* 
+	* Note we are using the meaningful Gui Object name, like btnAddShow or
+	* mntmHelpDialog as the "command string", just to keep things consistent
+	* and easy. And of course, we have moved all event processing to the child class.
+	* Separation of Form and Function into different classes.
+	**/
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//System.out.println( "ShowRunnerEvents::actionPerformed" );
+		// get the command string we set in the dialog
+	    String actionCommand = e.getActionCommand();
+
+	    //System.out.println( "ShowRunnerEvents::actionPerformed "+actionCommand );
+	    // build a dispatcher on those command strings, which are the names
+	    // of the gui items to keep it simple.
+	    // don't do code here, but call another method to actually do the work
+	    // to keep the dispatcher more readable.
+	    switch (actionCommand) {
+	        case "btnAddShow": 
+	    	    System.out.println( "ShowRunnerEvents::actionPerformed "+actionCommand );
+	        break;
+	        case "btnBrowseForShow": 
+	    	    System.out.println( "ShowRunnerEvents::actionPerformed "+actionCommand );
+	        break;
+	        case "mntmQuit": {
+	        	// do as little as possible
+	        	// allow the framework to do it all
+	        	dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+	        	
+	        }
+	        default:
+	        {
+	    	    System.out.println( "ShowRunnerEvents::Woops "+actionCommand );
+	        	break;
+	        }
+	    }
+	}
 }
+
+
--- src/com/ww/views/FirstWbGui.java
+++ src/com/ww/views/FirstWbGui.java
@@ -37,12 +37,17 @@ import java.awt.ScrollPane;
 import javax.swing.ListModel;
 import javax.swing.JScrollPane;
 import javax.swing.AbstractListModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 
 
+public class FirstWbGui extends JFrame implements ActionListener
 
-public class FirstWbGui extends JFrame {
+{
 
 	// get rid of a warning about serialization.
 	private static final long serialVersionUID = 19837502L;
@@ -57,6 +62,22 @@ public class FirstWbGui extends JFrame {
 	// very nice that this change does not mess up parsing by window builder
 	protected DefaultListModel<String>showList = new DefaultListModel<String>();
 	protected JList<String> listShows = new JList<String>(showList);
+	
+
+	
+	// we will overload this in the child class
+	// use the same one for every action
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String actionCommand = e.getActionCommand();
+	    System.out.println( "FirstWbGui::actionPerformed "+actionCommand );
+	}
+	
+	// catch window closing with our own method
+	// this is overridden in the child to save stuff before exit
+	public void windowClosingEvent(WindowEvent e) {
+		System.out.println( "FirstWbGui windowClosing" );
+	}
 
 	// The launch point is in the child class so just
 	// comment it out here.
@@ -81,6 +102,15 @@ public class FirstWbGui extends JFrame {
 	 * Create the frame.
 	 */
 	public FirstWbGui() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// Find ourselves in the great beyond
+				FirstWbGui win = (FirstWbGui)e.getWindow();
+				win.windowClosingEvent(e); // call us, which will call our child too
+			}
+		});
+		//addWindowListener(this);
 		setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		
 		System.out.println("FirstWbGui constructor reached");  
@@ -98,10 +128,14 @@ public class FirstWbGui extends JFrame {
 		menuBar.add(mnFile);
 		
 		JMenuItem mntmOpenShowList = new JMenuItem("Open Show List...");
+		mntmOpenShowList.setActionCommand("mntmOpenShowList");
+		mntmOpenShowList.addActionListener(this);
 		mntmOpenShowList.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		mnFile.add(mntmOpenShowList);
 		
 		JMenuItem mntmSaveShowList = new JMenuItem("Save Show List...");
+		mntmSaveShowList.setActionCommand("mntmSaveShowList");
+		mntmSaveShowList.addActionListener(this);
 		mntmSaveShowList.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		mnFile.add(mntmSaveShowList);
 		
@@ -109,6 +143,8 @@ public class FirstWbGui extends JFrame {
 		mnFile.add(separator);
 		
 		JMenuItem mntmQuit = new JMenuItem("Quit");
+		mntmQuit.setActionCommand("mntmQuit");
+		mntmQuit.addActionListener(this);
 		mntmQuit.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		mnFile.add(mntmQuit);
 		
@@ -117,12 +153,16 @@ public class FirstWbGui extends JFrame {
 		menuBar.add(mnHelp);
 		
 		JMenuItem mntmHelpDialog = new JMenuItem("Help...");
+		mntmHelpDialog.setActionCommand("mntmHelpDialog");
+		mntmHelpDialog.addActionListener(this);
 		mntmHelpDialog.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		mnHelp.add(mntmHelpDialog);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("About...");
-		mntmNewMenuItem.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnHelp.add(mntmNewMenuItem);
+		JMenuItem mntmAboutDialog = new JMenuItem("About...");
+		mntmAboutDialog.setActionCommand("mntmAboutDialog");
+		mntmAboutDialog.addActionListener(this);
+		mntmAboutDialog.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnHelp.add(mntmAboutDialog);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -140,6 +180,8 @@ public class FirstWbGui extends JFrame {
 		tfPathToImpress.setColumns(10);
 		
 		JButton btnBrowseForImpress = new JButton("...");
+		btnBrowseForImpress.setActionCommand("btnBrowseForImpress");
+		btnBrowseForImpress.addActionListener(this);
 		btnBrowseForImpress.setBounds(641, 79, 64, 38);
 		btnBrowseForImpress.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnBrowseForImpress);
@@ -167,11 +209,15 @@ public class FirstWbGui extends JFrame {
 		contentPane.add(txtShowPath);
 		
 		JButton btnBrowseForShow = new JButton("...");
+		btnBrowseForShow.setActionCommand("btnBrowseForShow");
+		btnBrowseForShow.addActionListener(this);
 		btnBrowseForShow.setBounds(515, 205, 55, 38);
 		btnBrowseForShow.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnBrowseForShow);
 		
 		JButton btnAddShow = new JButton("Add Show");
+		btnAddShow.setActionCommand("btnAddShow");
+		btnAddShow.addActionListener(this);
 		btnAddShow.setBounds(580, 205, 125, 38);
 		btnAddShow.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnAddShow);
@@ -182,21 +228,29 @@ public class FirstWbGui extends JFrame {
 		contentPane.add(ckbxBeepOnEnd);
 		
 		JButton btnRemoveAll = new JButton("Remove All");
+		btnRemoveAll.setActionCommand("btnRemoveAll");
+		btnRemoveAll.addActionListener(this);
 		btnRemoveAll.setBounds(580, 253, 125, 38);
 		btnRemoveAll.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnRemoveAll);
 		
 		JButton btnRemoveSelected = new JButton("Remove Selected");
+		btnRemoveSelected.setActionCommand("btnRemoveSelected");
+		btnRemoveSelected.addActionListener(this);
 		btnRemoveSelected.setBounds(411, 254, 165, 38);
 		btnRemoveSelected.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnRemoveSelected);
 		
 		JButton btnStartShows = new JButton("Start Shows");
+		btnStartShows.setActionCommand("btnStartShows");
+		btnStartShows.addActionListener(this);
 		btnStartShows.setBounds(365, 537, 165, 38);
 		btnStartShows.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnStartShows);
 		
 		JButton btnStopShows = new JButton("Stop Shows");
+		btnStopShows.setActionCommand("btnStopShows");
+		btnStopShows.addActionListener(this);
 		btnStopShows.setBounds(540, 537, 165, 38);
 		btnStopShows.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		contentPane.add(btnStopShows);
@@ -212,16 +266,22 @@ public class FirstWbGui extends JFrame {
 		contentPane.add(lblStatusLine);
 		
 		JButton btnMoveTop = new JButton("Top");
+		btnMoveTop.setActionCommand("btnMoveTop");
+		btnMoveTop.addActionListener(this);
 		btnMoveTop.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnMoveTop.setBounds(169, 253, 64, 38);
 		contentPane.add(btnMoveTop);
 		
 		JButton btnMoveUp = new JButton("Up");
+		btnMoveUp.setActionCommand("btnMoveUp");
+		btnMoveUp.addActionListener(this);
 		btnMoveUp.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnMoveUp.setBounds(243, 254, 64, 38);
 		contentPane.add(btnMoveUp);
 		
 		JButton btnMoveDown = new JButton("Down");
+		btnMoveDown.setActionCommand("btnMoveDown");
+		btnMoveDown.addActionListener(this);
 		btnMoveDown.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnMoveDown.setBounds(317, 254, 84, 38);
 		contentPane.add(btnMoveDown);

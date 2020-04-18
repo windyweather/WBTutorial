2cbc190c1178d27c5eac13f2048fd67f6599f243
Windy Player <windyplayer@gmx.com>
2020-04-18 14:45:46 -0700

    Save / Restore program defaults

--- src/com/ww/appcode/ShowRunnerEvents.java
+++ src/com/ww/appcode/ShowRunnerEvents.java
@@ -13,6 +13,14 @@ import javax.swing.DefaultListModel;
 
 import com.ww.views.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+//
+import java.io.IOException; 
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.*;
 
 /**
  * @author Darrell
@@ -46,7 +54,31 @@ public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 		lblStatusLine.setText( sts );
 	}
 
+	private boolean isOsWindows()
+	{
+		String osName = System.getProperty ("os.name");
+		if ( osName.contains("Windows") ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
+	private boolean isOsLinux()
+	{
+		String osName = System.getProperty ("os.name");
+		if ( osName.contains("Linux") ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	//
+	// Class Constructor
+	//
 	public ShowRunnerEvents() {
 		
 		System.out.println("ShowRunnerEvents constructor reached");
@@ -59,12 +91,12 @@ public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 	    	    System.out.println ("OS=" + s);
 		
 		// Set up some default values depending on the OS we find
-		String osName = System.getProperty ("os.name");
-		if ( osName.contains("Windows") ) {
+
+		if ( isOsWindows() ) {
 			tfPathToImpress.setText("C:\\Program Files\\LibreOffice\\program\\soffice.exe");
 			txtOptions.setText("--impress --show");
 		}
-		else if (osName.contains("Linux")) {
+		else if ( isOsLinux() ) {
 			tfPathToImpress.setText("soimpress");
 			txtOptions.setText("--show");
 		}
@@ -72,7 +104,7 @@ public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 		setStatus("ShowRunner Started");
 
 		// test our access to the show list
-		
+		/**
 		System.out.println( "showList size "+ showList.getSize() );
 		showList.removeAllElements();
 		System.out.println( "showList size "+ showList.getSize() );
@@ -128,11 +160,114 @@ public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 		System.out.println( "showList capacity "+ showList.capacity() );
 		showList.removeAllElements(); // leave the list empty
 		System.out.println( "showList empty list size "+ showList.size() );
+		 **/
+		
+		
+		String userDir = System.getProperty("user.home");
+		System.out.println("user.home "+ userDir);
 
-
+		// Restore the defaults to the gui
+		restoreDefaultsFile(); 
 		
 	}
 	
+	//
+	// use the java System.Properties class for ini files
+	// and write them in XML format
+	//
+	
+	private String propertyFilePathPrefix()
+	{
+		return System.getProperty("user.home")+File.separator+".windyweather";
+	}
+	
+	private String defaultsFilePath()
+	{
+		return propertyFilePathPrefix() + File.separator+"ImpressShowRunnerDefaults.xml";
+	}
+	
+	
+	//
+	// Restore the Defaults File from XML
+	//
+	private void restoreDefaultsFile()
+	{
+		String defaultsFile = defaultsFilePath();
+        Properties defProps = new Properties(); 
+        //FileOutputStream out = new FileOutputStream(defaultsFile); 
+		FileInputStream in;
+		try {
+			in = new FileInputStream(defaultsFile); 
+	        // load the properties from specified xml 
+	        defProps.loadFromXML(in); 
+	        in.close();
+        }
+        catch(IOException ex){
+            System.out.println("Defaults not found " + defaultsFile);
+            return;
+        }
+        
+        String sImpressPath = defProps.getProperty("ImpressPath");
+        String sImpressOptions = defProps.getProperty("ImpressOptions");
+        String sShowPath = defProps.getProperty("ShowPath");
+        
+        if ( 0 != sImpressPath.length() ) {
+        	tfPathToImpress.setText(sImpressPath);
+        }
+        if ( 0 != sImpressOptions.length() ) {
+        	txtOptions.setText(sImpressOptions);
+        }
+        if ( 0 != sShowPath.length() ) {
+        	txtShowPath.setText( sShowPath );
+        }
+        
+
+        System.out.println("restoreDefaultsFile "+defaultsFile);
+        defProps.list(System.out); 
+	}
+	
+	//
+	// store the gui to the defaults file
+	//
+	private void saveDefaultsFile()
+	{
+		String defaultsDir = propertyFilePathPrefix();
+		
+	    File directory = new File(defaultsDir);
+	    if (! directory.exists()){
+	    	System.out.println("saveDefaultsFile Creating defaults dir " + defaultsDir);
+	        directory.mkdir();
+	    	System.out.println("saveDefaultsFile Created defaults dir  " + defaultsDir);  
+	        // If you require it to make the entire directory path including parents,
+	        // use directory.mkdirs(); here instead.
+	    }
+		String defaultsFile = defaultsFilePath();
+        Properties defProps = new Properties(); 
+        FileOutputStream out;
+		try {
+			out = new FileOutputStream(defaultsFile);
+        }
+        catch(IOException ex){
+            System.out.println("saveDefaultsFile open failure " + defaultsFile);
+            return;
+        }
+
+        defProps.put("ImpressPath", tfPathToImpress.getText() ); 
+        defProps.put("ImpressOptions", txtOptions.getText() ); 
+        defProps.put("ShowPath", txtShowPath.getText() ); 
+        
+		try {
+	        // store the properties into specified xml 
+	        defProps.storeToXML(out, null);
+	        out.close();
+	        System.out.println("saveDefaultsFile");
+	        defProps.list(System.out); 
+        }
+        catch(IOException ex){
+            System.out.println("saveDefaultsFile write fail " + defaultsFile);
+            return;
+        }
+	}
 	
 	// catch the window closing event
 	// passed here with a nightmare, but hey, that's Java and AWT/Swing for you.
@@ -140,7 +275,9 @@ public class ShowRunnerEvents  extends FirstWbGui implements ActionListener{
 	public void windowClosingEvent(WindowEvent e) {
 		
 		System.out.println( "windowClosing - save your stuff here" );
-
+		// write the defaults file
+		saveDefaultsFile();
+		
 		// not necessary, we are closing, not closed
 		// the framework will exit for us. No need to hammer
 		// things down here.
